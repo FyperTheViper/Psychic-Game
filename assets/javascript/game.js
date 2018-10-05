6ec90185fda6f0044efe4bdba355f203b759848f
FyperTheViper <b.w.byars@gmail.com>
2018-10-05 15:03:57 -0700

    Image change on win works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,85 @@
+//variables
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guesses = [];
 var computerChoice;
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var spooky = new Audio("./assets/sounds/spooky.mp3")
-var image = document.getElementsByClassName("image2");
 
+//thank .split to divide up string faster
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+//very spooky sound that plays when win conditional is met
+var spooky = new Audio("./assets/sounds/spooky.mp3")
+
+//computer decides random letter here
 var randomLetter = function() {
-computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 };
 
+//counts down guess number
 var letterList = function() {
     document.getElementById("left").innerHTML = "Guesses Left: " + guessesLeft
 };
 
+//adds keys pushed and joins them with a comma
 var left = function() {
     document.getElementById("guesses").innerHTML = "Guesses so far: " + guesses.join(", ");
 };
 
+//reset function that refreshes appropriate areas once a win or loss is achieved
 function reset() {
     guessedLetters = [];
     guessesLeft = 9;
     guesses = [];
     randomLetter();
-    letterList()
-    left()
+    letterList();
+    left();
 };
 
-
+//where the magic happens, function starts when key is pressed
 document.onkeyup = function(event) {
 
+    //ensures that keys should work even during caps lock and checks if a valid (a-z) key is pushed
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var check = alphabet.includes(userGuess);
 
+    //prevents guesses from going below zero when a non valid key was pressed
     if (guessesLeft === 0) {
         reset();
     }
 
+    //counds down guess and invokes functions
     guessesLeft--;
     guesses.push(userGuess);
     letterList();
     left();
-    randomLetter()
+    randomLetter();
 
+    //finger wagging message for when non valid key is pressed
     if (check === false) {
         alert("Not a valid key, use a - z. (I thought you were psychic?)");
         return false;
     } else if (check === true) {
         left()
     
-    
+    //win conditional
     if ((guessesLeft > 0) && (userGuess === computerChoice)) {
+        //changes image upon win, with timeouts
+        document.querySelector('.eye1').style.display = 'none' 
+        document.querySelector('.eye2').style.display = 'block' 
+        setTimeout(function() { 
+            document.querySelector('.eye1').style.display = 'block' 
+            document.querySelector('.eye2').style.display = 'none' 
+        }, 1200);
         wins++;
         spooky.play();
         document.getElementById("wins").innerHTML = "Wins:      " + wins;
-        alert("You're right, " + computerChoice.toUpperCase() + " was my letter. Your psychic ability should be... developed.");
+        setTimeout(function() {
+            alert("You're right, " + computerChoice.toUpperCase() + " was my letter. Your psychic ability should be... developed.")
+        }, 500);
         reset();
+
+    //losing conditional
     } else if (guessesLeft == 0) {
         losses++;
         document.getElementById("losses").innerHTML = "Losses:      " + losses;
